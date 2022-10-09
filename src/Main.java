fe6466ec5c58669e3391009b2f267417cbfaf1ab
Andrey Kaizer <andre.kaizer@yandex.ru>
2022-10-10 00:45:50 +0300

    Реализация классов Main и Person

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,41 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        Queue<Person> queueAttraction = new LinkedList<>(generateClients());
+
+        while (!queueAttraction.isEmpty()) {
+            Person currentPerson = queueAttraction.poll();
+            System.out.printf("%s %s сошёл с аттракциона, имея - %d билет(а/ов)\n",
+                    currentPerson.getName(),
+                    currentPerson.getSureName(),
+                    currentPerson.getQuantityTickets());
+
+            int quantityTickets = currentPerson.getQuantityTickets();
+
+            if (quantityTickets > 0) {
+                System.out.printf("%s %s решил прокатиться на аттракционе, имея - %d билет(а/ов)\n",
+                        currentPerson.getName(),
+                        currentPerson.getSureName(),
+                        currentPerson.getQuantityTickets());
+
+                currentPerson.setQuantityTickets(--quantityTickets);
+                queueAttraction.offer(currentPerson);
+            }
+        }
+        System.out.println("Билеты закончились");
+    }
+
+    private static List<Person> generateClients() {
+        return List.of(
+                new Person("Ivan", "Tsarevich", 3),
+                new Person("Evpatiy", "Kolovrat", 4),
+                new Person("Alyosha", "Popovich", 1),
+                new Person("Ilya", "Muromets", 5),
+                new Person("Dobrynya", "Nikitich", 2));
+
+
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,33 @@
 public class Person {
+
+    private String name;
+    private String sureName;
+    private int quantityTickets;
+
+    public Person(String name, String sureName, int quantityTickets) {
+        this.name = name;
+        this.sureName = sureName;
+        this.quantityTickets = quantityTickets;
+
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public int getQuantityTickets() {
+        return quantityTickets;
+    }
+
+    public void setQuantityTickets(int quantityTickets) {
+        this.quantityTickets = quantityTickets;
+    }
+
 }
